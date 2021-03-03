db341b611b5858d2742518427ffab904b197c4af
Pepe García <pepe@pepegar.com>
2021-03-03 23:39:21 +0100

    prepare for class

--- exercises/bands.js
+++ exercises/bands.js
@@ -20,27 +20,3 @@ const bands = [
       }
     }
 ];
-
-let main = document.querySelector('.main');
-
-for (let band in bands) {
-    let bandDiv = document.createElement("div");
-
-    bandDiv.innerText = bands[band].name;
-
-    let components = bands[band].instruments
-
-    for (let component in components) {
-        let componentDiv = document.createElement("div");
-
-        componentDiv.classList.add("component");
-
-        componentDiv.innerText = component + " plays " + components[component];
-
-        bandDiv.appendChild(componentDiv);
-    }
-
-    console.log(bands[band]);
-
-    main.appendChild(bandDiv)
-}
