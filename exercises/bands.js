3e337572529b559839375407599480e30768f9cb
Pepe García <pepe@pepegar.com>
2021-03-06 16:54:29 +0100

    Add code from class

--- exercises/bands.js
+++ exercises/bands.js
@@ -1,22 +1,53 @@
 "use strict";
 
 const bands = [
-    {
-      name: "The Beatles",
-      instruments: {
-        John: "voice",
-        Paul: "bass",
-        Ringo: "drums",
-        George: "guitar"
-      }
-    },
-    {
-      name: "The Ramones",
-      instruments: {
-        Johnny: "voice",
-        Joey: "guitar",
-        Marky: "drums",
-        DeeDee: "bass"
-      }
+  {
+    name: "The Beatles",
+    instruments: {
+      John: "voice",
+      Paul: "bass",
+      Ringo: "drums",
+      George: "guitar"
     }
+  },
+  {
+    name: "The Ramones",
+    instruments: {
+      Johnny: "voice",
+      Joey: "guitar",
+      Marky: "drums",
+      DeeDee: "bass"
+    }
+  }
 ];
+
+// let main = document.querySelector(".main");
+
+// for (let i in bands) {
+//   let band = bands[i];
+//   let bandDiv = document.createElement("div");
+
+//   bandDiv.innerHTML = "<h2>" + band.name + "</h2>";
+
+//   for (let member in band.instruments) {
+
+//     let memberDiv = document.createElement("div");
+
+//     memberDiv.classList.add(band.instruments[member]);
+//     memberDiv.innerHTML = member;
+
+//     bandDiv.appendChild(memberDiv);
+
+//   }
+
+//   main.appendChild(bandDiv);
+// }
+
+
+let button = document.querySelector("#the-button");
+
+function eventReaction() {
+  document.title = "the button was clicked";
+}
+
+button.addEventListener('click', eventReaction);
--- exercises/events.js
+++ exercises/events.js
@@ -2,6 +2,34 @@
 
 // add a handler to button 1 so it changes the background of the page when clicked
 
+let button1 = document.querySelector("#button1");
+
+function handler1() {
+    let body = document.querySelector("body");
+    body.style.backgroundColor = "#000";
+    body.style.color = "#FFF";
+}
+
+button1.addEventListener('click', handler1);
+
 // add a handler to button 2 so it adds the text "dynamically created" to the <h1>
 
+let button2 = document.querySelector("#button2");
+
+function handler2() {
+    let h1 = document.querySelector("h1");
+    h1.innerHTML = "dinamically created";
+}
+
+button2.addEventListener('click', handler2);
+
 // add a handler to button 3 so it changes the color of the text in the title
+
+let button3 = document.querySelector("#button3");
+
+function handler3() {
+    let h1 = document.querySelector("h1");
+    h1.style.color = "red";
+}
+
+button3.addEventListener('click', handler3);
